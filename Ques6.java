c35e545749120e82c3de3f78f8134a22a3767d19
rishantjaisal <jaisalrishant@gmail.com>
2026-09-09 19:04:56 +0530

    Add SimpleObject class with constructor and main method

---
+++ Ques6.java
@@ -0,0 +1,12 @@
+package Java_OOPS_Program;
+
+class SimpleObject {
+    SimpleObject() {
+        System.out.println("Constructor of SimpleObject is called.");
+        System.out.println("Object created successfully.");
+    }
+
+    public static void main(String[] args) {
+        SimpleObject obj = new SimpleObject();
+    }
+}
